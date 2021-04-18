ee34c64ed93f65199c1a0563cc044970e6c28ae2
sound shell <sound_shell@outlook.com>
2021-04-18 12:01:56 +0800

    style:(index) Formatting

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
+import { fetch } from './src/fetch'
 export * from './src/checkStatus'
 export * from './src/interface/checkStatus.interface'
 export * from './src/interface/fetch.interface'
 export * from './src/interface/parseResponse.interface'
 export * from './src/parseResponse'
-import { fetch } from './src/fetch'
 
 export default fetch
